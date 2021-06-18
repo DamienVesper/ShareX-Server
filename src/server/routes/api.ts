bdf52009a325872d035148601f772f2626792c84
Damien Vesper <ldamienvesper@gmail.com>
2021-06-18 13:01:26 -0500

    merge

--- src/server/routes/api.ts
+++
@@ -1,4 +0,0 @@
-import Express from 'express';
-const router: Express.Router = Express.Router();
-
-export default router;
--- src/server/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import Express from 'express';
-const router: Express.Router = Express.Router();
-
-router.get(`/`, async (req: Express.Request, res: Express.Response) => res.render(`index.ejs`));
-
-export default router;
