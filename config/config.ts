984025115635867b12e4d54ac28cd0cf9644fa9d
Damien Vesper <ldamienvesper@gmail.com>
2021-06-18 13:00:16 -0500

    Revert "unused"
    This reverts commit 004ad46c683988e647acbc96dd525486acad507e.

---
+++ config/config.ts
@@ -0,0 +1,24 @@
+import { name, version } from '../package.json';
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+interface Args {
+    port: number
+}
+
+const argv = (yargs(hideBin(process.argv)).options({
+    port: { type: `number`, default: 8080 }
+}).argv as Args);
+
+const config = {
+    name,
+    port: argv.port,
+
+    version
+};
+
+export default config;
