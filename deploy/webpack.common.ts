431b1d3e99cc17a25b305afe5e6348a1fad2b885
Damien Vesper <ldamienvesper@gmail.com>
2021-06-18 10:50:14 -0500

    fix opts

--- deploy/webpack.common.ts
+++ deploy/webpack.common.ts
@@ -28,7 +28,9 @@ const config: Configuration = {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: `babel-loader`,
-                options: { presets: [`@babel/env`] }
+                options: {
+                    presets: [`@babel/preset-env`, `@babel/preset-react`]
+                }
             },
             {
                 test: /\.css$/,
