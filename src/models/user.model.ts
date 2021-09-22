9d7b67c09bdc4dd4cf3517515c81df74c90bf9a3
Damien Vesper <ldamienvesper@gmail.com>
2021-09-22 01:50:46 -0500

    cpp: init

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,14 +1,13 @@
 import * as Mongoose from 'mongoose';
 
 interface UserDoc extends Mongoose.Document {
-    username: string;
-    email: string;
-
+    id: string;
     token: string;
 }
 
 const userSchema = new Mongoose.Schema({
-
+    id: { type: String, required: true },
+    token: { type: String, required: false }
 });
 
 const User = Mongoose.model<UserDoc>(`User`, userSchema);
--- src/passport.ts
+++ src/passport.ts
@@ -1,8 +1,10 @@
 import passport from 'passport';
+import refresh from 'passport-oauth2-refresh';
 
 import { Strategy as DiscordStrategy } from 'passport-discord';
 import { VerifyCallback } from 'passport-oauth2';
-import refresh from 'passport-oauth2-refresh';
+
+import { User } from './models/user.model';
 
 const discordStrategy = new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
@@ -13,6 +15,12 @@ const discordStrategy = new DiscordStrategy({
 }, (accessToken: string, refreshToken: string, profile: DiscordStrategy.Profile, done: VerifyCallback) => {
     // eslint-disable-next-line
     (<any>profile).refreshToken = refreshToken;
+
+    const userExists = User.findOne({ discordID: profile.id });
+    if (!userExists) {
+        const user = new User({ discordId: profile.id });
+        user.save();
+    }
 });
 
 passport.use(discordStrategy);
