4b1ee9d62b34e4feb090b6103c9622b82f31b49e
Damien Vesper <ldamienvesper@gmail.com>
2021-09-21 22:21:08 -0500

    ban: just send 403 with no error pages

--- src/server/routes/ban.ts
+++ src/server/routes/ban.ts
@@ -4,13 +4,6 @@ import { Ban } from '../models/ban.model';
 
 const banRouter: Express.Router = Express.Router();
 
-banRouter.get(`*`, async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-    const isBanned = await Ban.findOne({ IP: req.ip });
-
-    if (isBanned) res.render(`errors/403.ejs`);
-    else next();
-});
-
 banRouter.all(`*`, async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
     const isBanned = await Ban.findOne({ IP: req.ip });
 
