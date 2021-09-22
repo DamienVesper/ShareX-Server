164b9374465d415aaa3c19749386ca8031bec85e
Damien Vesper <ldamienvesper@gmail.com>
2021-09-21 20:55:11 -0500

    utils: jsdoc

--- src/server/utils/randomInt.ts
+++ src/server/utils/randomInt.ts
@@ -1,3 +1,8 @@
+/**
+ * Generate a random integer.
+ * @param min The minimum amount specified for the range.
+ * @param max The maximum amount specified for the range.
+ */
 const randomInt = (min: number, max: number) => Math.floor(Math.random() * (max - min)) + min;
 
 export default randomInt;
--- src/server/utils/randomString.ts
+++ src/server/utils/randomString.ts
@@ -1,5 +1,9 @@
 import crypto from 'crypto';
 
+/**
+ * Generate a random string.
+ * @param length The length of the string.
+ */
 const randomString = (length: number) => crypto.randomBytes(length).toString(`hex`);
 
 export default randomString;
