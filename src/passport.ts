bfadcb528b47c591b3e4365e623417c50b47f7ea
Damien Vesper <ldamienvesper@gmail.com>
2021-09-22 20:09:26 -0500

    feat: setup routes for Express.js v4 with Passport middleware.

--- src/passport.ts
+++ src/passport.ts
@@ -1,5 +1,4 @@
 import passport from 'passport';
-import refresh from 'passport-oauth2-refresh';
 
 import { Strategy as DiscordStrategy } from 'passport-discord';
 import { VerifyCallback } from 'passport-oauth2';
@@ -11,19 +10,17 @@ const discordStrategy = new DiscordStrategy({
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: `https://i.alru.ga/auth/callback`,
     scope: [`identify`, `email`, `guilds`, `guilds.join`]
-    // eslint-disable-next-line
 }, (accessToken: string, refreshToken: string, profile: DiscordStrategy.Profile, done: VerifyCallback) => {
-    // eslint-disable-next-line
-    (<any>profile).refreshToken = refreshToken;
-
     const userExists = User.findOne({ discordID: profile.id });
     if (!userExists) {
         const user = new User({ discordId: profile.id });
-        user.save();
+        user.save(err => {
+            if (err) return done(err);
+            else return done(err, user);
+        });
     }
 });
 
 passport.use(discordStrategy);
-refresh.use(discordStrategy);
 
 export default passport;
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,13 @@
 import Express from 'express';
+
+import passport from '../passport';
+
 const router: Express.Router = Express.Router();
+
+// Discord Authentication & Callback
+router.get(`/discord`, passport.authenticate(`discord`));
+router.get(`/discord/callback`, passport.authenticate(`discord`, {
+    failureRedirect: `/`
+}), (req: Express.Request, res: Express.Response) => res.redirect(`/dashboard`));
 
 export default router;
