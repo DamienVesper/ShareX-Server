f2f71a50b59991249317ab7f49d6c6891e57bb34
Damien Vesper <ldamienvesper@gmail.com>
2021-09-24 22:09:08 -0500

    feat: tool to backup files

---
+++ tools/backupFiles.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+
+int main () {
+    const string filesDirectory = "/var/www/sharex/media";
+    const string backupDirectory = "/home/damienvesper/sharex/media";
+
+    char confirmBackup;
+
+    cout << "Are you sure you want to create a backup of the ShareX media? [Y/n] ";
+    cin >> confirmBackup;
+
+    if (confirmBackup != 'y') {
+        cout << "\nBackup aborted.";
+        return;
+    } else {
+        filesystem::copy(filesDirectory, backupDirectory);
+        cout << "\nSuccesfully backed up all media.";
+    }
+}
