14ac83f294becf29e583c19fc6bb14b2f2059d95
Damien Vesper <ldamienvesper@gmail.com>
2021-09-24 22:09:41 -0500

    fix: don't lint this line

--- src/routes/api/upload.ts
+++ src/routes/api/upload.ts
@@ -3,8 +3,6 @@ import Express from 'express';
 const router: Express.Router = Express.Router();
 
 // File uploads.
-router.post(`/files`, (req: Express.Request, res: Express.Response) => {
-    
-});
+// router.post(`/files`, (req: Express.Request, res: Express.Response) => {});
 
 export default router;
