1663ac37f25956bf0e70b903590b31214a953dac
Damien Vesper <ldamienvesper@gmail.com>
2021-09-26 10:20:57 -0500

    feat: create media folders if not existent already

---
+++ src/utils/createMediaFolders.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+
+import log from './log';
+
+/**
+ * Create media folders, if not present already.
+ */
+const createMediaFolders = (): void => {
+    log(`cyan`, `Checking if ShareX folders exist...`);
+
+    const mediaPath = `/var/www/sharex/i`;
+    const configPath = `/var/www/sharex/configs`;
+
+    if (!fs.existsSync(mediaPath)) {
+        log(`yellow`, `Media folder does not exist, creating...`);
+        fs.mkdirSync(mediaPath);
+    }
+
+    if (!fs.existsSync(configPath)) {
+        log(`yellow`, `Config folder does not exist, creating...`);
+        fs.mkdirSync(configPath);
+    }
+
+    log(`green`, `Finished checking all folders.`);
+};
+
+export default createMediaFolders;
