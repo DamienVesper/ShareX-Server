f2f925272ab68096ff8224a0749da5d9b0913a59
Damien Vesper <ldamienvesper@gmail.com>
2021-10-24 12:32:20 -0500

    pls help

--- src/routes/api/upload.ts
+++ src/routes/api/upload.ts
@@ -1,43 +1,57 @@
 import Express from 'express';
-import { UploadedFile } from 'express-fileupload';
+import { IncomingForm } from 'formidable';
+
+import * as path from 'path';
+import * as fs from 'fs';
 
 import config from '../../../config/config';
 
 import { Media } from '../../models/media.model';
 import { User } from '../../models/user.model';
 
-import * as path from 'path';
+import log from '../../utils/log';
 
 const router: Express.Router = Express.Router();
 
 // File uploads.
 router.post(`/files`, (req: Express.Request, res: Express.Response): void => {
-    if ((req.files?.file) == null) {
-        res.status(400);
-        return;
-    } else if ((req.query.key === undefined) && (req.body.key === undefined)) {
-        res.status(403);
-        return;
-    }
-
-    const authKey = (req.query.key as string);
-
-    void User.findOne({ token: authKey }).then(user => {
-        if ((user == null) || user.suspended) {
-            res.status(403);
+    const form = new IncomingForm();
+    form.parse(req, (err, fields: { key: string }, files) => {
+        if (err !== undefined) throw err;
+        if (files === undefined) {
+            res.status(400);
+            log(`red`, `there is no file`);
             return;
         }
 
-        const file = (req.files?.file as UploadedFile);
-        const media = new Media({ extension: path.extname(file.name) });
-        const fileName = media.name + media.extension;
+        const authKey = fields.key;
 
-        void media.save().then(() => {
-            void file.mv(path.resolve(`/var/www/sharex/i/`, fileName));
+        if (fields.key === undefined) {
+            res.status(400);
+            log(`red`, `there are no keys!`);
+            return;
+        }
 
-            res.status(200).jsonp({
-                success: true,
-                file: { url: `https://${config.domain}/i/${fileName}` }
+        void User.findOne({ token: authKey }).then(user => {
+            if ((user == null) || user.suspended) {
+                res.status(403);
+                return;
+            }
+
+            const file = ((files as unknown) as File);
+            const media = new Media({ extension: path.extname(file.name) });
+            const fileName = media.name + media.extension;
+
+            console.log(fileName);
+            console.log(file.name);
+
+            void media.save().then(() => {
+                void fs.rename(path.basename(fileName), path.resolve(`/var/www/sharex/i/`, fileName), () => {
+                    res.status(200).jsonp({
+                        success: true,
+                        file: { url: `https://${config.domain}/i/${fileName}` }
+                    });
+                });
             });
         });
     });
